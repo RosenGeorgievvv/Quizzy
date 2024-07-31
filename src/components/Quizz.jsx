0d06bd555c6098846c79608e93e854d3ca2d90a2
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-07-31 16:44:33 +0300

    Update Quizz.jsx

--- src/components/Quizz.jsx
+++ src/components/Quizz.jsx
@@ -1,51 +1,68 @@
-import React, { useState } from 'react'
-import { resultInitialState } from '../data';
+import React, { useState } from "react";
+import { resultInitialState } from "../data";
 
-const Quizz = ({questions}) => {
+const Quizz = ({ questions }) => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [answerIndex, setAnswerIndex] = useState(null);
+  const [choice, setChoice] = useState(null);
+  const [result, setResult] = useState(resultInitialState);
+  const [showResult, setShowResult] = useState(false);
 
-    const [currentQuestion, setCurrentQuestion] = useState(0);
-    const [answerIndex, setAnswerIndex] = useState(null);
-    const [choice, setChoice] = useState(null);
-    const [result, setResult] = useState(resultInitialState);
+  const { question, choices, correctAnswer } = questions[currentQuestion];
 
-    const {question, choices, correctAnswer} = questions[currentQuestion];
-
-    const onAnswerClick = (choice, index) =>{
-        setAnswerIndex(index);
-        if(choice === correctAnswer){
-            setChoice(true);
-        }else{
-            setChoice(false);
-        }
+  const onAnswerClick = (choice, index) => {
+    setAnswerIndex(index);
+    if (choice === correctAnswer) {
+      setChoice(true);
+    } else {
+      setChoice(false);
     }
+  };
 
-    const onClickNext = () =>{
-        setAnswerIndex(null);
+  const onClickNext = () => {
+    setAnswerIndex(null);
+    setResult((prev) =>
+      choice
+        ? {
+            ...prev,
+            score: prev.score + 5,
+            correctAnswers: prev.correctAnswers + 1,
+          }
+        : { ...prev, wrongAnswers: prev.wrongAnswers + 1 }
+    );
+    if(currentQuestion !== questions.length - 1){
+        setCurrentQuestion(currentQuestion + 1)
+    }else{
+        setCurrentQuestion(0);
+        setShowResult(true);
     }
+  };
 
   return (
-    <div className='quiz-container'>
-        <>
-        <span className='active-question-no'>{currentQuestion + 1}</span>
-        <span className='total-question'>/{questions.length}</span>
+    <div className="quiz-container">
+      <>
+        <span className="active-question-no">{currentQuestion + 1}</span>
+        <span className="total-question">/{questions.length}</span>
         <h2>{question}</h2>
         <ul>
-            {
-                choices.map((choice, index) =>(
-                    <li onClick={() => onAnswerClick(choice, index)} key={choice} className={answerIndex === index ? 'selected-answer' : null}>
-                       {choice}
-                    </li>
-                ))
-            }
+          {choices.map((choice, index) => (
+            <li
+              onClick={() => onAnswerClick(choice, index)}
+              key={choice}
+              className={answerIndex === index ? "selected-answer" : null}
+            >
+              {choice}
+            </li>
+          ))}
         </ul>
-        <div className='footer'>
-            <button onClick={onClickNext} disabled={answerIndex == null}>
-                {currentQuestion === questions.length - 1 ? "Finish" : "Next"}
-            </button>
+        <div className="footer">
+          <button onClick={onClickNext} disabled={answerIndex == null}>
+            {currentQuestion === questions.length - 1 ? "Finish" : "Next"}
+          </button>
         </div>
-        </>
+      </>
     </div>
-  )
-}
+  );
+};
 
-export default Quizz
+export default Quizz;

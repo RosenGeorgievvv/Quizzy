accc7c13bbb8efadbd5c44e7642ba4f5b7d7e1b0
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-07-31 16:51:39 +0300

    Update Quizz.jsx

--- src/components/Quizz.jsx
+++ src/components/Quizz.jsx
@@ -30,47 +30,61 @@ const Quizz = ({ questions }) => {
           }
         : { ...prev, wrongAnswers: prev.wrongAnswers + 1 }
     );
-    if(currentQuestion !== questions.length - 1){
-        setCurrentQuestion(currentQuestion + 1)
-    }else{
-        setCurrentQuestion(0);
-        setShowResult(true);
+    if (currentQuestion !== questions.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      setCurrentQuestion(0);
+      setShowResult(true);
     }
   };
 
+  const onTryAgain = () =>{
+    setResult(resultInitialState);
+    setShowResult(false);
+  }
+
   return (
     <div className="quiz-container">
-        {!showResult ? (
+      {!showResult ? (
         <>
-        <span className="active-question-no">{currentQuestion + 1}</span>
-        <span className="total-question">/{questions.length}</span>
-        <h2>{question}</h2>
-        <ul>
-          {choices.map((choice, index) => (
-            <li
-              onClick={() => onAnswerClick(choice, index)}
-              key={choice}
-              className={answerIndex === index ? "selected-answer" : null}
-            >
-              {choice}
-            </li>
-          ))}
-        </ul>
-        <div className="footer">
-          <button onClick={onClickNext} disabled={answerIndex == null}>
-            {currentQuestion === questions.length - 1 ? "Finish" : "Next"}
-          </button>
-        </div>
-      </>
+          <span className="active-question-no">{currentQuestion + 1}</span>
+          <span className="total-question">/{questions.length}</span>
+          <h2>{question}</h2>
+          <ul>
+            {choices.map((choice, index) => (
+              <li
+                onClick={() => onAnswerClick(choice, index)}
+                key={choice}
+                className={answerIndex === index ? "selected-answer" : null}
+              >
+                {choice}
+              </li>
+            ))}
+          </ul>
+          <div className="footer">
+            <button onClick={onClickNext} disabled={answerIndex == null}>
+              {currentQuestion === questions.length - 1 ? "Finish" : "Next"}
+            </button>
+          </div>
+        </>
       ) : (
-      <div className="result">
-        <h3>Result</h3>
-        <p>
+        <div className="result">
+          <h3>Result</h3>
+          <p>
             Total Questions: <span>{questions.length}</span>
-        </p>
+          </p>
+          <p>
+            Total Score: <span>{result.score}</span>
+          </p>
+          <p>
+            Correct Answers: <span>{result.correctAnswers}</span>
+          </p>
+          <p>
+            Wrong Answers: <span>{result.wrongAnswers}</span>
+          </p>
+          <button onClick={onTryAgain}>Try again</button>
         </div>
-        )}
-      
+      )}
     </div>
   );
 };

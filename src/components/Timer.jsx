fc6cc997d491013c25a33d3128ba1f05d376efea
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-08-01 12:29:29 +0300

    Update Timer.jsx

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,9 +1,28 @@
-import '../styles/timer.scss';
+import "../styles/timer.scss";
+import { useEffect, useState, useRef } from "react";
 
-function Timer(){
+function Timer({ duration }) {
+  const [counter, setCounter] = useState(0);
+  const [progress, setProgress] = useState(0);
+  const intervalRef = useRef();
 
-    return(
-        <div>Hello</div>
-    )
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      setCounter((curr) => curr + 1);
+    }, 1000);
+
+    //clear interval inside useEffect
+    return () => clearInterval(intervalRef.current);
+  }, []);
+
+  useEffect(() => {
+    setProgress(100 * (counter / duration));
+  }, [counter]);
+
+  return (
+    <div className="timer-container">
+      <div className="progress"></div>
+    </div>
+  );
 }
 export default Timer;
